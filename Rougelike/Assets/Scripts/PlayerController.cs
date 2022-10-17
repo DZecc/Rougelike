3b15300095683a956ec18ecedc71787769c6d691
DZecc <dalton.zecco@gmail.com>
2022-10-16 21:51:35 -0400

    lots of animations and roations to charater model

--- Rougelike/Assets/Scripts/PlayerController.cs
+++ Rougelike/Assets/Scripts/PlayerController.cs
@@ -9,11 +9,17 @@ public class PlayerController : MonoBehaviour
     
     public Rigidbody2D theRB;
 
+    public Transform gunArm;
+
+    private Camera theCam;
+
+    public Animator anim;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        theCam = Camera.main;
     }
 
     // Update is called once per frame
@@ -22,8 +28,37 @@ public class PlayerController : MonoBehaviour
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
 
+        moveInput.Normalize();
+
         //transform.position += new Vector3(moveInput.x * Time.deltaTime * moveSpeed, moveInput.y * Time.deltaTime * moveSpeed, 0f);
 
         theRB.velocity = moveInput * moveSpeed;
+
+
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 screenPoint = theCam.WorldToScreenPoint(transform.localPosition);
+
+        if(mousePos.x < screenPoint.x)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            gunArm.localScale = new Vector3(-1f, -1f, 1f);
+        }else
+        {
+            transform.localScale = Vector3.one;
+            gunArm.localScale = Vector3.one;
+        }
+
+        //rotate gun arm 
+        Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        gunArm.rotation = Quaternion.Euler(0, 0, angle);
+
+
+        if(moveInput != Vector2.zero)
+        {
+            anim.SetBool("isMoving", true);
+        } else {
+            anim.SetBool("isMoving", false);
+        }
     }
 }
