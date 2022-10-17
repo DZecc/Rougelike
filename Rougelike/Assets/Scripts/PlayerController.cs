f0d4ff726c6517ab109ecf97ca4d6bf7aef950d6
DZecc <dalton.zecco@gmail.com>
2022-10-16 23:51:16 -0400

    Added Shooting and effects

--- Rougelike/Assets/Scripts/PlayerController.cs
+++ Rougelike/Assets/Scripts/PlayerController.cs
@@ -15,6 +15,12 @@ public class PlayerController : MonoBehaviour
 
     public Animator anim;
 
+    public GameObject bulletToFire;
+    public Transform firePoint;
+
+    public float timeBetweenShots;
+    private float shotCounter;
+
 
     // Start is called before the first frame update
     void Start()
@@ -52,6 +58,29 @@ public class PlayerController : MonoBehaviour
         Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
         float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
         gunArm.rotation = Quaternion.Euler(0, 0, angle);
+
+        if(Input.GetMouseButtonDown(0))
+            {
+                Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
+            }
+        if(Input.GetMouseButton(0))
+        {
+            shotCounter -= Time.deltaTime;
+
+            if(shotCounter <= 0)
+            {
+                Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
+
+                shotCounter = timeBetweenShots;
+            }
+        }
+
+
+
+
+
+
+
 
 
         if(moveInput != Vector2.zero)
